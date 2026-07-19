05e804f7eb72417540c77c0f2b04485f2df19166
Eugen Holmes <baotuannguyen2307@gmail.com>
2026-07-20 02:26:40 +0700

    Add LCD

---
+++ App/Inc/app_lcd.h
@@ -0,0 +1,16 @@
+/*
+ * app_lcd.h
+ *
+ *  Created on: 30 thg 6, 2026
+ *      Author: Admin
+ */
+
+#ifndef INC_APP_LCD_H_
+#define INC_APP_LCD_H_
+
+#include "FreeRTOS.h"
+#include "task.h"
+
+void Task_LCD_Handler(void *pvParameters);
+
+#endif /* INC_APP_LCD_H_ */
